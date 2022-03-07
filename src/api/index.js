08c37fe27fc2c3155fa8e25f9f0d84dc0decb13a
Yago <yago.vinii764@gmail.com>
2022-03-07 00:35:19 -0300

    Initial Commit

---
+++ src/api/index.js
@@ -0,0 +1,6 @@
+var five = require("johnny-five");//incluindo a API e suas funcionalidades
+var board = new five.Board(); // criamdo o objeto do tipo placa que é o arduino
+
+board.on("ready", function() { //ligando a leitura do arduino via JS
+    //TODO:
+});
